b800b0977bd5f3526d19b474ae17b0d2e98933db
Ashok Gunji <21ne5a0225@gmail.com>
2025-10-19 18:42:06 +0530

    added database configaration for the Rag chatbot

--- ragchatbot/src/app/chat/ragchatbot.tsx
+++ ragchatbot/src/app/chat/ragchatbot.tsx
@@ -1,41 +1,85 @@
 "use client";
-import { Conversation, ConversationContent, ConversationScrollButton } from '@/components/ai-elements/conversation';
-import { PromptInput, PromptInputBody, PromptInputSubmit, PromptInputTextarea, PromptInputToolbar, PromptInputTools } from '@/components/ai-elements/prompt-input';
-import React, { useState ,Fragment} from 'react'
+import {
+  Conversation,
+  ConversationContent,
+  ConversationScrollButton,
+} from "@/components/ai-elements/conversation";
+import {
+  PromptInput,
+  PromptInputBody,
+  PromptInputMessage,
+  PromptInputSubmit,
+  PromptInputTextarea,
+  PromptInputToolbar,
+  PromptInputTools,
+} from "@/components/ai-elements/prompt-input";
+import React, { useState, Fragment } from "react";
 import { useChat } from "@ai-sdk/react";
+import { Message, MessageContent } from "@/components/ai-elements/message";
+import { Response } from "@/components/ai-elements/response";
+import { Loader } from "@/components/ai-elements/loader";
 
-const RagChatBot:React.FC = () => {
+const RagChatBot: React.FC = () => {
+  const [input, setInput] = useState<string>("");
 
-    const [input,setInput] = useState<string>("");
+  const handleSubmit = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(event.target.value);
+  };
 
-    const handleSubmit = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setInput(event.target.value);
+  const handleSubmits = (message: PromptInputMessage) => {
+    if (!message.text) {
+      return;
     }
+    sendMessage({
+      text: message.text,
+    });
+    setInput("");
+  };
 
-    const{messages,sendMessage,status} =useChat();
+  const { messages, sendMessage, status } = useChat();
   return (
-
-    <div className='max-w-4xl mx-auto p-6 relative size-full h-[calc(100vh)]'>
-        <div className='flex flex-col h-full'>
-            <Conversation className='h-full'>
-                <ConversationContent > Message will go here </ConversationContent>
-                <ConversationScrollButton/>
-            </Conversation>
-            <PromptInput className='mt-4'>
-                <PromptInputBody>
-                    <PromptInputTextarea value={input} onChange={handleSubmit}/>
-                </PromptInputBody>
-                <PromptInputToolbar>
-                    <PromptInputTools>
-                        {/* Model selector will go here */}
-                    </PromptInputTools>
-                    <PromptInputSubmit/>
-                </PromptInputToolbar>
-            </PromptInput>
-        </div>
-      
+    <div className="max-w-4xl mx-auto p-6 relative size-full h-[calc(100vh)]">
+      <div className="flex flex-col h-full">
+        <Conversation className="h-full">
+          <ConversationContent>
+            {messages.map((message) => (
+              <div key={message.id}>
+                {message.parts.map((part, i) => {
+                  switch (part.type) {
+                    case "text":
+                      return (
+                        <Fragment key={`${message.id}-${i}`}>
+                          <Message from={message.role}>
+                            <MessageContent>
+                              <Response>{part.text}</Response>
+                            </MessageContent>
+                          </Message>
+                        </Fragment>
+                      );
+                    default:
+                      return null;
+                  }
+                })}
+              </div>
+            ))}
+            {(status === "submitted" || status === "streaming") && <Loader />}
+          </ConversationContent>
+          <ConversationScrollButton />
+        </Conversation>
+        <PromptInput onSubmit={handleSubmits} className="mt-4">
+          <PromptInputBody>
+            <PromptInputTextarea value={input} onChange={handleSubmit} />
+          </PromptInputBody>
+          <PromptInputToolbar>
+            <PromptInputTools>
+              {/* Model selector will go here */}
+            </PromptInputTools>
+            <PromptInputSubmit />
+          </PromptInputToolbar>
+        </PromptInput>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default RagChatBot;
